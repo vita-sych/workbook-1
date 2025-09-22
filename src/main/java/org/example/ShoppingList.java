65fd8337d228401a43d0df7fc34f9c9d58a62471
VitaSych <vita.sych.vs@gmail.com>
2025-09-22 14:58:26 -0700

    add ShoppingList class

---
+++ src/main/java/org/example/ShoppingList.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        String[] shoppingList = {"tomato", "orange", "cucumber", "juice", "yogurt", "bread", "water", "chicken", "beef", "cheese"};
+
+        for (String item : shoppingList) {
+            System.out.println(item);
+        }
+    }
+}
