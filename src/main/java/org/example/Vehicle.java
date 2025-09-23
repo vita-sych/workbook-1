9377b3960097939e7edc00d18a20fb84ec7baa3e
VitaSych <vita.sych.vs@gmail.com>
2025-09-23 11:44:13 -0700

    add Vehicle class with variables

---
+++ src/main/java/org/example/Vehicle.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Vehicle {
+    int identificationNumber = 1234567;
+    String model = "Ford";
+    String color = "red";
+    boolean hasTowingPackage = true;
+    int odometerReading = 72000;
+    int price = 25000;
+    char qualityRating = 'A';
+    String phoneNumber = "4254397865";
+    String ssn = "453678902";
+    String zipCode = "98004";
+}
